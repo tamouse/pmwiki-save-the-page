6230c21f80553e95fe2dd4676b1d5c1662bf150b
Tamara Temple <tamouse@gmail.com>
2012-11-25 20:01:03 -0600

    classify complete, ready for merge

--- Tests/SaveThePageTest.php
+++ Tests/SaveThePageTest.php
@@ -15,31 +15,108 @@ class SaveThePageTest extends PHPUnit_Framework_TestCase
 
     public function testCompress()
     {
-    	$this->assertEquals("Save the page", SaveThePage::compress("Save \n the    \t \r page"));
+        $this->assertTrue(method_exists("SaveThePage", "compress"));
+        $cases = array(
+            array("content"  => "Save \n the     \t page \r some \r\n more",
+                  "expected" => 'Save the page some more'),
+            array("content"  => "function ()
+                                 {
+                                    var a,
+                                        b,
+                                        d = 'something';
+                                    u = f + '&url=' + en(l.href);
+                                 };",
+                  "expected" => "function(){var a,b,d='something';u=f+'&url='+en(l.href);};"));
+        foreach ($cases as $case) {
+            $this->assertEquals($case['expected'], SaveThePage::compress($case['content']));    
+        }
+
+    	
     }
 
     public function testBookmarlet()
     {
-        $b = SaveThePage::bookmarklet("../savethepage/bookmarklet.js");
-        $this->assertNotEmpty($b);
-        echo "$b\n";
+        $this->assertTrue(method_exists("SaveThePage", "bookmarklet"));
+        $pagename = "/tmp/".__CLASS__.".".__METHOD__.".testfile";
+        $content = <<<EOF
+function ()
+{
+    var a,
+        b,
+        d=document;
+    u = f + '&url=' + en(l.href);
+};
+EOF;
+        $expected = "<a href=\"javascript:function(){var a,b,d=document;u=f+'&url='+en(l.href);};\">Bookmarklet</a>";
+        file_put_contents($pagename, $content);
+        $b = SaveThePage::bookmarklet($pagename);
+        unlink($pagename);
+        $this->assertEquals($expected,$b);
     }
     
     public function testCleanTitle()
     {
+        $this->assertTrue(method_exists("SaveThePage", "cleantitle"));
         $this->assertEquals("Tamwiki Main Tamwiki Home Page", SaveThePage::cleantitle("TamWiki » Main — TamWiki Home Page") );
     }
 
     public function testFetchPage()
     {
-        $content = "This is some content";
-        file_put_contents("/tmp/testfile.1234", $content);
-        $this->assertEquals($content, SaveThePage::fetchpage("file:///tmp/testfile.1234"));
+        $this->assertTrue(method_exists("SaveThePage", "fetchpage"));
+        $this->assertNotEmpty(SaveThePage::fetchpage("http://www.example.com/"));
+        $this->assertFalse(SaveThePage::fetchpage("http://asdflaksdfjasdfasfdasdf.com"));
+        $this->assertFalse(SaveThePage::fetchpage("file:///etc/password"));
     }
 
     public function testConvertEncoding ()
     {
+        $this->assertTrue(method_exists("SaveThePage", "convertencoding"));
         $html = "Some ¶ “text that contains highbyte chars”";
         $this->assertEquals("Some &para; &ldquo;text that contains highbyte chars&rdquo;",SaveThePage::convertencoding($html));
+    }
+
+    public function testFilter ()
+    {
+        $this->assertTrue(method_exists("SaveThePage", "filter"));
+        $cases = array(
+            array('content'   => 'Just some text',
+                  'expected'  => array(0=>'Just some text',1=>'',2=>0),
+                  'filter'    => '/bin/cat',
+                  'options'   => null),
+            array('content'   => 'just some text',
+                  'expected'  => array(0=>'',1=>"sh: asdfxxxxxxxx: not found\n",2=>127),
+                  'filter'    => 'asdfxxxxxxxx',
+                  'options'   => null)
+            );
+        foreach ($cases as $case) {
+            $results = SaveThePage::filter($case['content'], $case['filter'], $case['options']);
+            $this->assertTrue(is_array($results));
+            $this->assertEquals(6,count($results));
+            //var_dump($results);
+            //var_dump($case['expected'][0]);
+            //$exp = ($case['expected'][0]);
+            $this->assertEquals(($case['expected'][0]), $results[0]);
+            $this->assertEquals(($case['expected'][1]), $results[1]);
+            $this->assertEquals(($case['expected'][2]), $results[2]);
+        }
+    }
+
+    public function testGetBaseUrl ()
+    {
+        $this->assertTrue(method_exists("SaveThePage", "getbaseurl"));
+        $cases = array(
+            array('content'  => 'http://www.example.com/with/some/path',
+                  'expected' => 'http://www.example.com:80/'),
+            array('content'  => '/some/page.html',
+                  'expected' => 'http://www.example.com:80/')
+                      );
+        foreach ($cases as $case) {
+            if (!array_key_exists('HTTP_HOST', $_SERVER)) {
+                $_SERVER['HTTP_HOST'] = 'www.example.com';
+            }
+            $result = SaveThePage::getbaseurl($case['content']);
+            $this->assertTrue(is_string($result));
+            $this->assertEquals($case['expected'], $result);
+        }
     }
 }
--- savethepage/SaveThePage.php
+++ savethepage/SaveThePage.php
@@ -32,16 +32,14 @@ class SaveThePage
      *
      * @return string compressed bookmarklet code.
      */
-    function bookmarklet($bookmarkletfn, $format='<a href="javascript:$bookmarklet">Bookmaklet</a>')
+    function bookmarklet($bookmarkletfn, $format='<a href="javascript:$bookmarklet">Bookmarklet</a>')
     {
         $replacements['$bookmarklet'] = self::compress(file_get_contents($bookmarkletfn));
         return str_replace(array_keys($replacements), array_values($replacements), $format);
     }
 
-        /**
+    /**
      * cleantitle
-     * 
-     * @param string $title The page title from the web page.
      *
      * @access public
      *
@@ -67,36 +65,16 @@ class SaveThePage
      */
     function compress($s)
     {
-        return preg_replace('/[[:space:]]+/', ' ', $s);
-    }
-
-    /**
-     * fetchpage
-     * 
-     * @param string $url Uniform Resource Locator of page to fetch.
-     *
-     * @access public
-     *
-     * @return string Fetched page content.
-     */
-    function fetchpage ($url)
-    {
-      $ch = curl_init();
-      $c_options = array(
-        CURLOPT_URL              => $url,
-        CURLOPT_USERAGENT        => "Mozilla/5.0",
-        CURLOPT_HEADER           => false,
-        CURLOPT_FOLLOWLOCATION   => true,
-        CURLOPT_AUTOREFERER      => true,
-        CURLOPT_MAXREDIRS        => 10,
-        CURLOPT_CONNECTTIMEOUT   => 30,
-        CURLOPT_TIMEOUT          => 120,
-        CURLOPT_RETURNTRANSFER   => true,
-        );
-      curl_setopt_array($ch,$c_options);
-      $contents = curl_exec($ch);
-      curl_close($ch);
-      return $contents;
+        $s = preg_replace('/(\r|\n|\r\n)/', '', $s);
+        $s = preg_replace('/[[:space:]]+/', ' ', $s);
+        $s = preg_replace('/ = /','=', $s);
+        $s = preg_replace('/, /', ',', $s);
+        $s = preg_replace('/ \+ /', '+', $s);
+        $s = preg_replace('/ *{ */', '{', $s);
+        $s = preg_replace('/ \(/', '(', $s);
+        $s = preg_replace('/\) /', ')', $s);
+        $s = preg_replace('/; /', ';', $s);
+        return $s;
     }
 
     /**
@@ -110,15 +88,125 @@ class SaveThePage
      */
     function convertencoding ($html)
     {
-      if (empty($html)) {
-        return FALSE;
-      }
-
       $e=mb_detect_encoding($html);
       if (FALSE === $e) {
         $e = 'ISO-8859-1';
       }
       return mb_convert_encoding($html,'HTML-ENTITIES',$e);
     }
+
+    /**
+     * fetchpage
+     * 
+     * @param string $url Uniform Resource Locator of page to fetch.
+     *
+     * @access public
+     *
+     * @return mixed Fetched page content as string, or false on failure.
+     */
+    function fetchpage ($url)
+    {
+      $ch = curl_init();
+      $c_options = array(
+        CURLOPT_URL              => $url,
+        CURLOPT_USERAGENT        => "Mozilla/5.0",
+        CURLOPT_HEADER           => false,
+        CURLOPT_FOLLOWLOCATION   => true,
+        CURLOPT_AUTOREFERER      => true,
+        CURLOPT_MAXREDIRS        => 10,
+        CURLOPT_CONNECTTIMEOUT   => 5,
+        CURLOPT_TIMEOUT          => 10,
+        CURLOPT_RETURNTRANSFER   => true,
+        CURLOPT_PROTOCOLS        => CURLPROTO_HTTP |
+                                    CURLPROTO_HTTPS |
+                                    CURLPROTO_FTP |
+                                    CURLPROTO_FTPS,
+        CURLOPT_REDIR_PROTOCOLS  => CURLPROTO_HTTP |
+                                    CURLPROTO_HTTPS |
+                                    CURLPROTO_FTP |
+                                    CURLPROTO_FTPS,
+        );
+      curl_setopt_array($ch,$c_options);
+      $contents = curl_exec($ch);
+      curl_close($ch);
+      return $contents;
+    }
+
+    /**
+     * filter
+     * 
+     * @param string $text String to run through filter
+     * @param string $filter Filter command
+     * @param mixed $options Options to append to $filter to run command
+     *          (Note: may be string or array, array will be imploded into a string separated by spaces)
+     * 
+     * @access public
+     *
+     * @return array [output, errors, return]
+     **/
+    function filter ($text,$filter,$options=null)
+    {
+        if (is_array($options)) {
+            $options = implode(" ", $options);
+        }
+        $dspec = array(
+            0 => array('pipe','r'), // STDIN
+            1 => array('pipe','w'), // STDOUT
+            2 => array('pipe','w'), // STDERR
+            );
+
+        $process = proc_open("$filter $options", $dspec, $pipes);
+
+        if (is_resource($process)) {
+            // $pipes now looks like this:
+            // 0 => writeable handle connected to child stdin
+            // 1 => readable handle connected to child stdout
+            // 2 => readable handle connected to child stderr
+
+            fwrite($pipes[0], $text);
+            fclose($pipes[0]);
+
+            $output = stream_get_contents($pipes[1]);
+            fclose($pipes[1]);
+
+            $errors = stream_get_contents($pipes[2]);
+            fclose($pipes[2]);
+
+            // It is important that you close any pipes before calling
+            // proc_close in order to avoid a deadlock
+            $return_value = proc_close($process);
+        } else {
+            throw new Exception("Could not create process!", 1);
+        }
+        return array(0 => $output,        'output' => $output,
+                     1 => $errors,        'errors' => $errors,
+                     2 => $return_value,  'return' => $return_value
+                    );
+    }
+    /**
+     * Determine the page's base url
+     *
+     * @param string $url
+     * 
+     * @access public
+     *  
+     * @return string base url
+     **/
+    function getbaseurl ($url)
+    {
+      if (empty($url)) return $url;
+      
+      $url_components = parse_url($url);
+      
+      if (!array_key_exists('scheme',$url_components)) $url_components['scheme'] = 'http';
+      if (!array_key_exists('host',$url_components)) $url_components['host'] = $_SERVER['HTTP_HOST'];
+      if (!array_key_exists('port',$url_components)) $url_components['port'] = "80";
+      $baseurl = sprintf("%s://%s:%s/",
+                 $url_components['scheme'],
+                 $url_components['host'],
+                 $url_components['port']);
+      return $baseurl;
+
+    } // END function STP_GetBaseUrl
 
 }
